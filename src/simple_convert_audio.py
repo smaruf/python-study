29b1aca9d5b41ec8ecfff2dba01b2ba7eff17aec
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2022-09-18 17:06:46 +0600

    Simple audio convert 220918-1659

---
+++ src/simple_convert_audio.py
@@ -0,0 +1,12 @@
+## pip install pydub
+## apt-get install ffmpeg
+from os import path
+from pydub import AudioSegment
+
+# files                                                                         
+src = "transcript.mp3"
+dst = "test.wav"
+
+# convert wav to mp3                                                            
+sound = AudioSegment.from_mp3(src)
+sound.export(dst, format="wav")
