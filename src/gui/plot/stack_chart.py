aea21c887302ca34761dda9f9cd5786bf57b2529
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2022-09-22 13:07:03 +0600

    Sample Plot stack-chart 220922-1307

---
+++ src/gui/plot/stack_chart.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+import matplotlib.pyplot as plt
+
+data1 = [23,85, 72, 43, 52]
+data2 = [42, 35, 21, 16, 9]
+plt.bar(range(len(data1)), data1)
+plt.bar(range(len(data2)), data2, bottom=data1)
+plt.show()
