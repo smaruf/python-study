3b977d0cda8fd01b27a8e8e9f9a43aa9dd35922d
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2023-09-07 16:45:53 +0200

    Create sample_1.py 230907-1645

---
+++ src/diagram/sample_1.py
@@ -0,0 +1,11 @@
+# using pip (pip3)
+#$ pip install diagrams
+from diagrams import Diagram
+from diagrams.aws.compute import EC2
+from diagrams.aws.database import RDS
+from diagrams.aws.network import ELB
+
+with Diagram("Web Service", show=False):
+    ELB("lb") >> EC2("web") >> RDS("userdb")
+
+# run: python sample_1.py
