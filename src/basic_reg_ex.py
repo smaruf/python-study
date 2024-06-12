4ef90b50c326884bb8e56393edcf87669c361ebe
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2024-06-12 13:25:16 +0200

    Update basic_reg_ex.py

--- src/basic_reg_ex.py
+++ src/basic_reg_ex.py
@@ -1,21 +1,14 @@
 import re
 # Search for a pattern in a string
-text = "The quick brown fox jumps
-over the lazy dog"
+text = "The quick brown fox jumps over the lazy dog"
 match = re.search(r"brown", text)
 if match:
   print("Match found!")
-# Find all occurrences of a pattern
-in a string
-text = "The quick brown fox jumps
-over the lazy dog"
-matches = re.findall(r"the", text,
-re.IGNORECASE)
+# Find all occurrences of a pattern in a string
+text = "The quick brown fox jumps over the lazy dog"
+matches = re.findall(r"the", text, re.IGNORECASE)
 print(matches)
-# Replace all occurrences of a
-pattern in a string
-text = "The quick brown fox jumps
-over the lazy dog"
-new_text = re.sub(r"the", "that",
-text, flags=re.IGNORECASE)
+# Replace all occurrences of a pattern in a string
+text = "The quick brown fox jumps over the lazy dog"
+new_text = re.sub(r"the", "that", text, flags=re.IGNORECASE)
 print(new_text)
