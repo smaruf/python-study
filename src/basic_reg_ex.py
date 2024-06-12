a21d1a21a71aa51be52dca3e3e8b4342c7df145b
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2024-06-12 13:22:52 +0200

    Create basic_reg_ex.py 20240611

---
+++ src/basic_reg_ex.py
@@ -0,0 +1,21 @@
+import re
+# Search for a pattern in a string
+text = "The quick brown fox jumps
+over the lazy dog"
+match = re.search(r"brown", text)
+if match:
+  print("Match found!")
+# Find all occurrences of a pattern
+in a string
+text = "The quick brown fox jumps
+over the lazy dog"
+matches = re.findall(r"the", text,
+re.IGNORECASE)
+print(matches)
+# Replace all occurrences of a
+pattern in a string
+text = "The quick brown fox jumps
+over the lazy dog"
+new_text = re.sub(r"the", "that",
+text, flags=re.IGNORECASE)
+print(new_text)
