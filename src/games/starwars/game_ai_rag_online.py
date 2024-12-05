501975f4ea6f594d4b644c9872c3d0e4dace4a9c
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2024-12-05 19:32:22 +0100

    Create game_ai_rag_online.py 241205

---
+++ src/games/starwars/game_ai_rag_online.py
@@ -0,0 +1,5 @@
+# Need to access openai for suggestion
+# Need observe multiplayers and oponent reaction and counter reaction
+# Need create RAG and store as file/db/json for future use
+# Need to training facility
+# Need to practice game ownself using ai and simulate out come
