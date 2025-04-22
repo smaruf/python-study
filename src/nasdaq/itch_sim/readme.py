6d616ece55f322b2e817ba2bf9edcadf7e23e1db
MShamsul Maruf <m.shamsul.maruf.itask@gmail.com>
2025-04-22 13:01:12 +0200

    Create readme.py

---
+++ src/nasdaq/itch_sim/readme.py
@@ -0,0 +1,12 @@
+## ITCH protocol data with Kafka and WebUI
+
+```bash
+itch_sim/
+├── itch_kafka_producer.py
+├── itch_kafka_consumer_web.py
+├── itch_template.py
+└── web_ui/
+    ├── index.html
+    └── script.js
+
+```
